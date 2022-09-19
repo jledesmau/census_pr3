a21c39db04978f2d8bd73053ea2419545950d91d
Jorge Ledesma <jledesmau@gmail.com>
2022-09-19 00:34:06 -0400

    API Tests

--- test_functions.py
+++ test_functions.py
@@ -5,6 +5,13 @@ import pandas as pd
 from src.ml.model import compute_model_metrics, inference
 from src.ml.data import process_data
 
+from fastapi.testclient import TestClient
+import json
+from main import app
+
+# Instantiate the testing client with our app
+client = TestClient(app)
+
 # Setting-up data and model objects
 @pytest.fixture
 def raw_data():
@@ -71,3 +78,51 @@ def test_inference(processed_data, model):
     # Testing type and size of inference outputs
     assert predictions.shape[0] == processed_data[0].shape[0]
     assert type(predictions) == np.ndarray
+
+# Tests for API
+def test_get_greeting():
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json() == {"message": "Welcome to The Census Salary Classifier API"}
+
+def test_post_inference_0():
+    data = {
+        "age": 39,
+        "workclass": "State-gov",
+        "fnlgt": 77516,
+        "education": "Bachelors",
+        "education-num": 13,
+        "marital-status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 2174,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States"
+    }
+    r = client.post("/inference", data = json.dumps(data))
+    assert r.status_code == 200
+    assert r.json() == {"prediction": 0}
+
+def test_post_inference_1():
+    data = {
+        "age": 39,
+        "workclass": "State-gov",
+        "fnlgt": 77516,
+        "education": "Bachelors",
+        "education-num": 13,
+        "marital-status": "Never-married",
+        "occupation": "Adm-clerical",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Male",
+        "capital-gain": 217400,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States"
+    }
+    r = client.post("/inference", data = json.dumps(data))
+    assert r.status_code == 200
+    assert r.json() == {"prediction": 1}
