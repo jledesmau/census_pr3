4b38916625cfb47334629d75f0425664ee0b9996
Jorge Ledesma <jledesmau@gmail.com>
2022-09-19 01:35:49 -0400

    Project complete

--- app_requests.py
+++ app_requests.py
@@ -1,7 +1,26 @@
 import requests
+import json
 
-url = "https://census-pr3.herokuapp.com/"
+url = "https://census-pr3.herokuapp.com/inference"
 
-response = requests.get(url)  # This should return the response we defined
-print(response.status_code)
-print(response.json())
+data = {
+    "age": 39,
+    "workclass": "State-gov",
+    "fnlgt": 77516,
+    "education": "Bachelors",
+    "education-num": 13,
+    "marital-status": "Never-married",
+    "occupation": "Adm-clerical",
+    "relationship": "Not-in-family",
+    "race": "White",
+    "sex": "Male",
+    "capital-gain": 217400,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States"
+}
+
+response = requests.post(url, data = json.dumps(data))
+print("Querying live API")
+print("Status code: " + str(response.status_code))
+print("Response: " + str(response.json()))
